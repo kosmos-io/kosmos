ff38dce6887d572d01fbc24c4b9aee954bb3891f
wuyingjun <wuyingjun@cmss.chinamobile.com>
2023-08-11 13:53:00 +0800

    init e2e test
    Co-authored-by: wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>
    Co-authored-by: hanweisen <hanweisen_yewu@cmss.chinamobile.com>
    Signed-off-by: baoyinghai <baoyinghai_yewu@cmss.chinamobile.com>

---
+++ test/e2e/elector_test.go
@@ -0,0 +1,57 @@
+package e2e
+
+import (
+	"context"
+	"github.com/onsi/gomega"
+
+	"github.com/onsi/ginkgo/v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	clusterlinkv1alpha1 "cnp.io/clusterlink/pkg/apis/clusterlink/v1alpha1"
+	"cnp.io/clusterlink/pkg/utils/role"
+)
+
+var _ = ginkgo.Describe("elector testing", func() {
+	ginkgo.Context("gateway role add test", func() {
+		ginkgo.It("Check if gateway role gateway role is set correctly", func() {
+			gomega.Eventually(func(g gomega.Gomega) (bool, error) {
+				clusterNodes, err := clusterLinkClient.ClusterlinkV1alpha1().ClusterNodes().List(context.TODO(), metav1.ListOptions{})
+				if err != nil {
+					return false, err
+				}
+				return hasGatewayRoleOnly(clusterNodes.Items), nil
+			}, 60, 10).Should(gomega.Equal(true))
+		})
+	})
+})
+
+func hasGatewayRoleOnly(nodes []clusterlinkv1alpha1.ClusterNode) bool {
+	clusterToNodeMap := make(map[string][]clusterlinkv1alpha1.ClusterNode)
+
+	// Group the nodes by cluster name
+	for _, node := range nodes {
+		clusterName := node.Spec.ClusterName
+		if len(clusterName) > 0 {
+			if _, ok := clusterToNodeMap[clusterName]; !ok {
+				clusterToNodeMap[clusterName] = []clusterlinkv1alpha1.ClusterNode{node}
+			} else {
+				clusterToNodeMap[clusterName] = append(clusterToNodeMap[clusterName], node)
+			}
+		}
+	}
+
+	// Check if each cluster has only one node with GatewayRole
+	for _, nodes := range clusterToNodeMap {
+		gatewayCount := 0
+		for _, node := range nodes {
+			if role.HasRole(node, clusterlinkv1alpha1.RoleGateway) {
+				gatewayCount++
+			}
+		}
+		if gatewayCount != 1 {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ test/e2e/framework/cluster.go
@@ -0,0 +1,110 @@
+package framework
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+
+	clusterlinkv1alpha1 "cnp.io/clusterlink/pkg/apis/clusterlink/v1alpha1"
+	"cnp.io/clusterlink/pkg/generated/clientset/versioned"
+	"cnp.io/clusterlink/projectpath"
+)
+
+func FetchClusters(client versioned.Interface) ([]clusterlinkv1alpha1.Cluster, error) {
+	clusters, err := client.ClusterlinkV1alpha1().Clusters().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return clusters.Items, nil
+}
+
+func LoadRESTClientConfig(kubeconfig string, context string) (*rest.Config, error) {
+	loader := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}
+	loadedConfig, err := loader.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	if context == "" {
+		context = loadedConfig.CurrentContext
+	}
+	klog.Infof("Use context %v", context)
+
+	return clientcmd.NewNonInteractiveClientConfig(
+		*loadedConfig,
+		context,
+		&clientcmd.ConfigOverrides{},
+		loader,
+	).ClientConfig()
+}
+
+func CreateCluster(clusterName, podCIDR, serviceCIDR string) error {
+	command := fmt.Sprintf("source %s/hack/cluster.sh && create_cluster %s %s %s", projectpath.Root, clusterName, podCIDR, serviceCIDR)
+	err := runCmd("bash", "-c", command)
+	return err
+}
+
+func Join(hostCluster, memberCluster string) error {
+	command := fmt.Sprintf("source %s/hack/cluster.sh && join_cluster %s %s", projectpath.Root, hostCluster, memberCluster)
+	err := runCmd("bash", "-c", command)
+	return err
+}
+
+func DeleteCluster(memberCluster string) error {
+	command := fmt.Sprintf("source %s/hack/cluster.sh && delete_cluster %s", projectpath.Root, memberCluster)
+	err := runCmd("bash", "-c", command)
+	return err
+}
+
+func LoadClusterLink(memberCluster string) error {
+	command := fmt.Sprintf("source %s/hack/cluster.sh && load_clusterlink_images %s", projectpath.Root, memberCluster)
+	err := runCmd("bash", "-c", command)
+	return err
+}
+
+func runCmd(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	stdoutIn, err := cmd.StdoutPipe()
+	if err != nil {
+		klog.Errorf("runCmd get stdout err %v", err)
+		return err
+	}
+	stderrIn, err := cmd.StderrPipe()
+	if err != nil {
+		klog.Errorf("runCmd get stderr err %v", err)
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// read from stdout and stderr in background and log any output
+	go func() {
+		scannerOut := bufio.NewScanner(stdoutIn)
+		for scannerOut.Scan() {
+			log.Println(scannerOut.Text())
+		}
+	}()
+	go func() {
+		scannerErr := bufio.NewScanner(stderrIn)
+		for scannerErr.Scan() {
+			log.Println(scannerErr.Text())
+		}
+	}()
+
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
