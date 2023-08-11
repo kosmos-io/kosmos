871b10d0c26b1eb5c9a14c8dd5b913486b1bc47a
wuyingjun <wuyingjun@cmss.chinamobile.com>
2023-08-11 11:28:34 +0800

    add cluster and node controller
    Signed-off-by: wangqi <wangqi_yewu@cmss.chinamobile.com>

---
+++ pkg/controllers/cluster/cluster_controller.go
@@ -0,0 +1,368 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	calicoclient "github.com/projectcalico/calico/libcalico-go/lib/clientv3"
+	"github.com/projectcalico/calico/libcalico-go/lib/options"
+	cwatch "github.com/projectcalico/calico/libcalico-go/lib/watch"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog/v2"
+
+	clusterlinkv1alpha1 "cnp.io/clusterlink/pkg/apis/clusterlink/v1alpha1"
+	"cnp.io/clusterlink/pkg/generated/clientset/versioned"
+	"cnp.io/clusterlink/pkg/utils"
+	"cnp.io/clusterlink/pkg/utils/flags"
+	"cnp.io/clusterlink/pkg/utils/keys"
+)
+
+type SetClusterPodCIDRFun func(cluster *clusterlinkv1alpha1.Cluster) error
+
+type Controller struct {
+	// RateLimiterOptions is the configuration for rate limiter which may significantly influence the performance of
+	// the Controller.
+	RateLimiterOptions   flags.Options
+	clusterName          string
+	processor            utils.AsyncWorker
+	kubeClient           *kubernetes.Clientset
+	dynamicClient        *dynamic.DynamicClient
+	podLister            v1.PodLister
+	clusterLinkClient    *versioned.Clientset
+	setClusterPodCIDRFun SetClusterPodCIDRFun
+	stopCh               <-chan struct{}
+}
+
+func NewController(clusterName string, kubeClient *kubernetes.Clientset, dynamicClient *dynamic.DynamicClient, clusterLinkClient *versioned.Clientset) *Controller {
+	return &Controller{
+		clusterName:       clusterName,
+		kubeClient:        kubeClient,
+		clusterLinkClient: clusterLinkClient,
+		dynamicClient:     dynamicClient,
+	}
+}
+
+func (c *Controller) onChange(obj interface{}) {
+	runtimeObj, ok := obj.(runtime.Object)
+	if !ok {
+		return
+	}
+	c.processor.Enqueue(runtimeObj)
+}
+
+func (c *Controller) Start(ctx context.Context) error {
+	klog.Infof("Starting cluster Controller.")
+	c.stopCh = ctx.Done()
+
+	opt := utils.Options{
+		Name: "cluster Controller",
+		KeyFunc: func(obj interface{}) (utils.QueueKey, error) {
+			return keys.ClusterWideKeyFunc(obj)
+		},
+		ReconcileFunc:      c.Reconcile,
+		RateLimiterOptions: c.RateLimiterOptions,
+	}
+	c.processor = utils.NewAsyncWorker(opt)
+
+	factory := informers.NewSharedInformerFactory(c.kubeClient, 0)
+	informer := factory.Core().V1().Pods().Informer()
+	c.podLister = factory.Core().V1().Pods().Lister()
+	podFilterFunc := func(pod *corev1.Pod) bool {
+		//TODO 确认这个写法是否正确
+		return pod.Labels["component"] == "kube-apiserver"
+	}
+
+	cluster, err := c.clusterLinkClient.ClusterlinkV1alpha1().Clusters().Get(ctx, c.clusterName, metav1.GetOptions{})
+	if err != nil {
+		klog.Errorf("can not find local cluster %s, err: %v", c.clusterName, err)
+		return err
+	}
+	_, err = informer.AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: func(obj interface{}) bool {
+			pod := obj.(*corev1.Pod)
+			return podFilterFunc(pod)
+		},
+		Handler: cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				c.onChange(cluster)
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				c.onChange(cluster)
+			},
+		},
+	})
+	if err != nil {
+		klog.Errorf("can not add handler err: %v", err)
+		return err
+	}
+	isEtcd := CheckIsEtcd(cluster)
+	if !isEtcd {
+		c.setClusterPodCIDRFun, err = c.initCalicoInformer(ctx, cluster, c.dynamicClient)
+		if err != nil {
+			klog.Errorf("cluster %s initCalicoInformer err: %v", err)
+			return err
+		}
+	} else {
+		c.setClusterPodCIDRFun, err = c.initCalicoWatcherWithEtcdBackend(ctx, cluster)
+		if err != nil {
+			klog.Errorf("cluster %s initCalicoWatcherWithEtcdBackend err: %v", err)
+			return err
+		}
+	}
+	factory.Start(c.stopCh)
+	factory.WaitForCacheSync(c.stopCh)
+	c.processor.Run(1, c.stopCh)
+
+	return nil
+}
+
+func (c *Controller) Reconcile(key utils.QueueKey) error {
+	clusterWideKey, ok := key.(keys.ClusterWideKey)
+	if !ok {
+		klog.Error("invalid key")
+		return fmt.Errorf("invalid key")
+	}
+	namespacedName := types.NamespacedName{
+		Name:      clusterWideKey.Name,
+		Namespace: clusterWideKey.Namespace,
+	}
+	reconcileCluster, err := c.clusterLinkClient.ClusterlinkV1alpha1().Clusters().Get(context.Background(), c.clusterName, metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil
+		}
+		klog.Errorf("get %s error: %v", namespacedName, err)
+		return err
+	}
+	// sync service cidr
+	pods, err := c.podLister.Pods("kube-system").List(labels.Everything())
+	if err != nil {
+		klog.Errorf("list pod for cluster %s err: %v", reconcileCluster.Name, err)
+		return err
+	}
+	var serviceCIDRS []string
+	for i := range pods {
+		pod := pods[i]
+		if isApiServer(pod) {
+			serviceCIDRS, err = ResolveServiceCIDRs(pod)
+			if err != nil {
+				klog.Errorf("get %s service cidr error: %v", pod.Name, err)
+				continue
+			}
+			break
+		}
+	}
+	if serviceCIDRS == nil || len(serviceCIDRS) == 0 {
+		klog.Errorf("resolve serviceCIDRS for cluster %s failure", c.clusterName)
+		return err
+	}
+	// sync pod cidr
+	err = c.setClusterPodCIDRFun(reconcileCluster)
+	if err != nil {
+		klog.Errorf("cluster %s sync pod cidr err: %v", reconcileCluster.GetName(), err)
+		return err
+	}
+
+	reconcileCluster.Status.ServiceCIDRs = serviceCIDRS
+	//TODO use sub resource
+	_, err = c.clusterLinkClient.ClusterlinkV1alpha1().Clusters().Update(context.TODO(), reconcileCluster, metav1.UpdateOptions{})
+	if err != nil {
+		klog.Errorf("could not update cluster %s, err: %v", reconcileCluster.GetName(), err)
+		return err
+	}
+	return nil
+}
+
+func (c *Controller) initCalicoInformer(context context.Context, cluster *clusterlinkv1alpha1.Cluster, dynamicClient dynamic.Interface) (SetClusterPodCIDRFun, error) {
+	//TODO 这里应该判断cluster的cni插件类型，如果是calico才去观测ippool事件，否则可能都没有ippool这个资源对象，watch一个不存在的资源对象可能会导致这里报错
+	dynamicInformerFactory := dynamicinformer.NewDynamicSharedInformerFactory(dynamicClient, 0)
+	gvr := schema.GroupVersionResource{
+		Group:    "crd.projectcalico.org",
+		Version:  "v1",
+		Resource: "ippools",
+	}
+	ippoolInformer := dynamicInformerFactory.ForResource(gvr)
+	_, err := ippoolInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			runtimeObject, ok := obj.(runtime.Object)
+			if !ok {
+				return
+			}
+			metaInfo, err := meta.Accessor(runtimeObject)
+			if err != nil {
+				return
+			}
+			if !strings.HasPrefix(metaInfo.GetName(), utils.ExternalIPPoolNamePrefix) {
+				c.onChange(cluster)
+			}
+
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			runtimeObject, ok := newObj.(runtime.Object)
+			if !ok {
+				return
+			}
+			metaInfo, err := meta.Accessor(runtimeObject)
+			if err != nil {
+				return
+			}
+			if !strings.HasPrefix(metaInfo.GetName(), utils.ExternalIPPoolNamePrefix) {
+				c.onChange(cluster)
+			}
+
+		},
+		DeleteFunc: func(obj interface{}) {
+			runtimeObject, ok := obj.(runtime.Object)
+			if !ok {
+				return
+			}
+			metaInfo, err := meta.Accessor(runtimeObject)
+			if err != nil {
+				return
+			}
+			if !strings.HasPrefix(metaInfo.GetName(), utils.ExternalIPPoolNamePrefix) {
+				c.onChange(cluster)
+			}
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	ippoolLister := ippoolInformer.Lister()
+	dynamicInformerFactory.Start(context.Done())
+	dynamicInformerFactory.WaitForCacheSync(context.Done())
+
+	return func(cluster *clusterlinkv1alpha1.Cluster) error {
+		ippools, err := ippoolLister.List(labels.Everything())
+		if err != nil {
+			return err
+		}
+		podCIDRS := make([]string, 0, 5)
+		for i := range ippools {
+			p := ippools[i]
+			pool, ok := p.(*unstructured.Unstructured)
+			if ok {
+				name := pool.GetName()
+				if !strings.HasPrefix(name, utils.ExternalIPPoolNamePrefix) {
+					obj := pool.Object
+					cidr := utils.GetCIDRs(obj)
+					if len(cidr) > 0 {
+						podCIDRS = append(podCIDRS, cidr)
+					}
+				}
+			}
+		}
+		cluster.Status.PodCIDRs = podCIDRS
+		return nil
+	}, nil
+}
+
+func (c *Controller) initCalicoWatcherWithEtcdBackend(ctx context.Context, cluster *clusterlinkv1alpha1.Cluster) (SetClusterPodCIDRFun, error) {
+	calicoClient, err := GetCalicoClient(cluster)
+	if err != nil {
+		klog.Errorf("failed to get calico kubeClient %s err: %v", cluster.Name, err)
+		return nil, err
+	}
+
+	watch, err := createIppoolWatcher(calicoClient, cluster)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+	mainLoop:
+		for {
+			select {
+			case <-ctx.Done():
+				klog.Info("Context is done. Stop watching ippool in cluster %s", cluster.Name)
+				watch.Stop()
+				break mainLoop
+			case event, ok := <-watch.ResultChan():
+				if !ok {
+					// If the channel is closed then resync/recreate the watch.
+					klog.Info("Watch channel closed by remote - recreate watcher")
+					watch.Stop()
+					watch, err = createIppoolWatcher(calicoClient, cluster)
+					if err != nil {
+						klog.Errorf("create watcher for cluster %s err: %v", cluster.Name, err)
+						klog.Info("sleep 10s retry")
+						time.Sleep(10 * time.Second)
+					}
+				}
+				switch event.Type {
+				case cwatch.Error:
+					klog.Errorf("receive err %v from cluster %s", err, cluster.Name)
+					watch.Stop()
+					watch, err = createIppoolWatcher(calicoClient, cluster)
+					if err != nil {
+						klog.Errorf("create watcher for cluster %s err: %v", cluster.Name, err)
+						klog.Info("sleep 10s retry")
+						time.Sleep(10 * time.Second)
+					}
+				case cwatch.Deleted:
+					c.onChange(cluster)
+				default:
+					ippool, ok := event.Object.(*v3.IPPool)
+					if ok {
+						if validIPPool(ippool) {
+							c.onChange(cluster)
+						}
+					} else {
+						typeOfObject := reflect.TypeOf(event.Object)
+						klog.Errorf("event object from cluster %s is not ippool type, get: %s", cluster.Name, typeOfObject.Name())
+					}
+				}
+			}
+		}
+	}()
+	return func(cluster *clusterlinkv1alpha1.Cluster) error {
+		ippools, err := calicoClient.IPPools().List(context.Background(), options.ListOptions{})
+		if err != nil {
+			klog.Errorf("list ippools from cluster %s err: %v", cluster.Name, err)
+			return err
+		}
+		podCIDRs := make([]string, 0, 5)
+		for i := range ippools.Items {
+			ippool := ippools.Items[i]
+			if validIPPool(&ippool) {
+				podCIDRs = append(podCIDRs, ippool.Spec.CIDR)
+			}
+		}
+		cluster.Status.PodCIDRs = podCIDRs
+		return nil
+	}, nil
+}
+
+func createIppoolWatcher(calicoClient calicoclient.Interface, cluster *clusterlinkv1alpha1.Cluster) (cwatch.Interface, error) {
+	watch, err := calicoClient.IPPools().Watch(context.TODO(), options.ListOptions{})
+	if err != nil {
+		klog.Errorf("init calico ippool watch for cluster %s err: %v", cluster.Name, err)
+		return nil, err
+	}
+	return watch, nil
+}
+
+func validIPPool(ippool *v3.IPPool) bool {
+	return ippool.Spec.Disabled == false && !strings.HasPrefix(utils.ExternalIPPoolNamePrefix, ippool.Name)
+}
+
+func isApiServer(pod *corev1.Pod) bool {
+	return pod.Namespace == "kube-system" && strings.HasPrefix(pod.Name, "kube-apiserver")
+}
---
+++ pkg/controllers/node/node_controller.go
@@ -0,0 +1,235 @@
+package node
+
+import (
+	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/util/errors"
+	"net"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	clusterlinkv1alpha1 "cnp.io/clusterlink/pkg/apis/clusterlink/v1alpha1"
+	"cnp.io/clusterlink/pkg/generated/clientset/versioned"
+	interfacepolicy "cnp.io/clusterlink/pkg/utils/interface-policy"
+)
+
+const (
+	controllerName = "node-controller"
+	RequeueTime    = 10 * time.Second
+	clusterLabel   = "clusterlink.io/cluster"
+)
+
+type Reconciler struct {
+	client.Client
+	Scheme            *runtime.Scheme
+	ClusterLinkClient *versioned.Clientset
+	ClusterName       string
+}
+
+var predicatesFunc = predicate.Funcs{
+	CreateFunc: func(createEvent event.CreateEvent) bool {
+		return true
+	},
+	UpdateFunc: func(updateEvent event.UpdateEvent) bool {
+		return true
+	},
+	DeleteFunc: func(deleteEvent event.DeleteEvent) bool {
+		return true
+	},
+	GenericFunc: func(genericEvent event.GenericEvent) bool {
+		return true
+	},
+}
+
+func (r *Reconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	klog.Infof("node controller starts to reconcile cluster %s", request.NamespacedName.Name)
+
+	clusterNodeName := ClusterNodeName(r.ClusterName, request.Name)
+	var node corev1.Node
+	if err := r.Get(ctx, request.NamespacedName, &node); err != nil {
+		if apierrors.IsNotFound(err) {
+			err := r.ClusterLinkClient.ClusterlinkV1alpha1().ClusterNodes().Delete(ctx, clusterNodeName, metav1.DeleteOptions{})
+			if err != nil {
+				klog.Warningf("delete cluster node %s err: %v", clusterNodeName, err)
+			}
+			return reconcile.Result{}, nil
+		}
+		klog.Errorf("get %s error: %v", request.NamespacedName, err)
+		return reconcile.Result{RequeueAfter: RequeueTime}, nil
+	}
+
+	if !node.GetDeletionTimestamp().IsZero() {
+		err := r.ClusterLinkClient.ClusterlinkV1alpha1().ClusterNodes().Delete(ctx, clusterNodeName, metav1.DeleteOptions{})
+		if err != nil {
+			klog.Warningf("delete cluster node %s err: %v", node.Name, err)
+		}
+		return reconcile.Result{}, nil
+	}
+
+	// add or update clusternode
+	var internalIP string
+	var internalIP6 string
+	for _, address := range node.Status.Addresses {
+		if address.Type == corev1.NodeInternalIP {
+			_, proto := ParseIP(address.Address)
+			if proto == 4 && len(internalIP) == 0 {
+				internalIP = address.Address
+			}
+			if proto == 6 && len(internalIP6) == 0 {
+				internalIP6 = address.Address
+			}
+			if len(internalIP) > 0 && len(internalIP6) > 0 {
+				break
+			}
+		}
+	}
+	clusterNode := &clusterlinkv1alpha1.ClusterNode{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: clusterNodeName,
+			Labels: map[string]string{
+				clusterLabel: r.ClusterName,
+			},
+		},
+	}
+	cluster, err := r.ClusterLinkClient.ClusterlinkV1alpha1().Clusters().Get(ctx, r.ClusterName, metav1.GetOptions{})
+	if err != nil {
+		klog.Errorf("get cluster %s err: %v", r.ClusterName, err)
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	err = CreateOrUpdateClusterNode(r.ClusterLinkClient, clusterNode, func(n *clusterlinkv1alpha1.ClusterNode) error {
+		n.Spec.NodeName = node.Name
+		n.Spec.ClusterName = r.ClusterName
+		n.Spec.IP = internalIP
+		n.Spec.IP6 = internalIP6
+		n.Spec.InterfaceName = interfacepolicy.GetInterfaceName(cluster.Spec.NICNodeNames, clusterNode.Spec.NodeName, cluster.Spec.DefaultNICName)
+		return nil
+	})
+	if err != nil {
+		klog.Errorf("can nod update or create clusterNode %s err: %v", clusterNode.Name, err)
+		return reconcile.Result{Requeue: true}, nil
+	}
+	return reconcile.Result{}, nil
+}
+
+type MutateClusterNodeFn func(node *clusterlinkv1alpha1.ClusterNode) error
+
+func CreateOrUpdateClusterNode(client versioned.Interface, node *clusterlinkv1alpha1.ClusterNode, f MutateClusterNodeFn) error {
+	clusterNode, err := client.ClusterlinkV1alpha1().ClusterNodes().Get(context.Background(), node.Name, metav1.GetOptions{})
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			return err
+		}
+		if err := f(node); err != nil {
+			return err
+		}
+		_, err := client.ClusterlinkV1alpha1().ClusterNodes().Create(context.Background(), node, metav1.CreateOptions{})
+		if err != nil {
+			return err
+		} else {
+			return nil
+		}
+	}
+	if err := f(clusterNode); err != nil {
+		return err
+	}
+	_, err = client.ClusterlinkV1alpha1().ClusterNodes().Update(context.Background(), clusterNode, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Reconciler) CleanOrphan() error {
+	k8sNodeList := &corev1.NodeList{}
+	if err := r.List(context.Background(), k8sNodeList); err != nil {
+		return err
+	}
+	k8sNodeNameSet := make(map[string]struct{})
+	for _, node := range k8sNodeList.Items {
+		k8sNodeNameSet[node.Name] = struct{}{}
+	}
+
+	clusterNodes, err := r.ClusterLinkClient.ClusterlinkV1alpha1().ClusterNodes().List(context.Background(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", clusterLabel, r.ClusterName),
+	})
+	if err != nil {
+		return err
+	}
+	var errs []error
+	cnt := 0
+	for _, cn := range clusterNodes.Items {
+		clusterNode := cn
+		if _, ok := k8sNodeNameSet[cn.Spec.NodeName]; !ok {
+			if err := r.Delete(context.Background(), &clusterNode); err != nil {
+				errs = append(errs, err)
+				klog.Warningf("failed to delete clusterNode %s", clusterNode.Name)
+			} else {
+				cnt++
+			}
+		}
+	}
+	if len(errs) != 0 {
+		return errors.NewAggregate(errs)
+	}
+	if cnt > 0 {
+		klog.Infof("successfully deleted %d orphan clusterNodes", cnt)
+	}
+
+	return nil
+}
+
+func (r *Reconciler) SetupWithManager(mgr manager.Manager, stopChan <-chan struct{}) error {
+	if r.Client == nil {
+		r.Client = mgr.GetClient()
+	}
+
+	ticker := time.Tick(60 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker:
+				err := r.CleanOrphan()
+				if err != nil {
+					klog.Warningf("clear orphan err: %v", err)
+				}
+			case <-stopChan:
+				return
+			}
+		}
+	}()
+	return ctrl.NewControllerManagedBy(mgr).
+		Named(controllerName).
+		WithOptions(controller.Options{}).
+		For(&corev1.Node{}, builder.WithPredicates(predicatesFunc)).
+		Complete(r)
+}
+
+func ClusterNodeName(clusterName, nodeName string) string {
+	return clusterName + "-" + nodeName
+}
+
+func ParseIP(s string) (net.IP, int) {
+	ip := net.ParseIP(s)
+	if ip == nil {
+		return nil, 0
+	}
+	ipv4 := ip.To4()
+	if ipv4 != nil {
+		return ipv4, 4
+	}
+	return ip, 6
+}
