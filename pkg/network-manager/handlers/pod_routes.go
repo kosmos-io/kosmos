384962eb7b439d0faf457cf648c7101e287b2b41
wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>
2023-08-23 16:14:50 +0800

    fix: network manager lint errors
    Signed-off-by: wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>

--- pkg/network-manager/handlers/pod_routes.go
+++ pkg/network-manager/handlers/pod_routes.go
@@ -30,55 +30,59 @@ func (h *PodRoutes) Do(c *Context) (err error) {
 			podCIDRs = cluster.Status.PodCIDRs
 		}
 
-		otherClusterNodes := c.Filter.GetAllNodesExceptCluster(target.Spec.ClusterName)
-
-		for _, cidr := range podCIDRs {
-			ipType := helpers.GetIPType(cidr)
-
-			var vxBridge string
-			var vxLocal string
-			if ipType == helpers.IPV6 {
-				vxBridge = constants.VXLAN_BRIDGE_NAME_6
-				vxLocal = constants.VXLAN_LOCAL_NAME_6
-			} else if ipType == helpers.IPV4 {
-				vxBridge = constants.VXLAN_BRIDGE_NAME
-				vxLocal = constants.VXLAN_LOCAL_NAME
-			}
-
-			targetDev := c.GetDeviceFromResults(target.Name, vxBridge)
-			targetIP, _, err := net.ParseCIDR(targetDev.Addr)
-			if err != nil {
-				klog.Warning("PodRoutesHandler, cannot parse target dev addr, nodeName: %s, devName: %s", target.Name, vxBridge)
-				continue
-			}
-
-			for _, n := range otherClusterNodes {
-				srcCluster := c.Filter.GetClusterByName(n.Spec.ClusterName)
-				if n.IsGateway() || srcCluster.IsP2P() {
-					c.Results[n.Name].Routes = append(c.Results[n.Name].Routes, v1alpha1.Route{
-						CIDR: cidr,
-						Gw:   targetIP.String(),
-						Dev:  vxBridge,
-					})
-					continue
-				}
-
-				gw := c.Filter.GetGatewayNodeByClusterName(n.Spec.ClusterName)
-				gwDev := c.GetDeviceFromResults(gw.Name, vxLocal)
-				gwIP, _, err := net.ParseCIDR(gwDev.Addr)
-				if err != nil {
-					klog.Warning("PodRoutesHandler, cannot parse gw dev addr, nodeName: %s, devName: %s", gw.Name, vxLocal)
-					continue
-				}
-
-				c.Results[n.Name].Routes = append(c.Results[n.Name].Routes, v1alpha1.Route{
-					CIDR: cidr,
-					Gw:   gwIP.String(),
-					Dev:  vxLocal,
-				})
-			}
-		}
+		BuildRoutes(c, target, podCIDRs)
 	}
 
 	return
+}
+
+func BuildRoutes(ctx *Context, target *v1alpha1.ClusterNode, cidrs []string) {
+	otherClusterNodes := ctx.Filter.GetAllNodesExceptCluster(target.Spec.ClusterName)
+
+	for _, cidr := range cidrs {
+		ipType := helpers.GetIPType(cidr)
+
+		var vxBridge string
+		var vxLocal string
+		if ipType == helpers.IPV6 {
+			vxBridge = constants.VXLAN_BRIDGE_NAME_6
+			vxLocal = constants.VXLAN_LOCAL_NAME_6
+		} else if ipType == helpers.IPV4 {
+			vxBridge = constants.VXLAN_BRIDGE_NAME
+			vxLocal = constants.VXLAN_LOCAL_NAME
+		}
+
+		targetDev := ctx.GetDeviceFromResults(target.Name, vxBridge)
+		targetIP, _, err := net.ParseCIDR(targetDev.Addr)
+		if err != nil {
+			klog.Warning("cannot parse target dev addr, nodeName: %s, devName: %s", target.Name, vxBridge)
+			continue
+		}
+
+		for _, n := range otherClusterNodes {
+			srcCluster := ctx.Filter.GetClusterByName(n.Spec.ClusterName)
+			if n.IsGateway() || srcCluster.IsP2P() {
+				ctx.Results[n.Name].Routes = append(ctx.Results[n.Name].Routes, v1alpha1.Route{
+					CIDR: cidr,
+					Gw:   targetIP.String(),
+					Dev:  vxBridge,
+				})
+				continue
+			}
+
+			gw := ctx.Filter.GetGatewayNodeByClusterName(n.Spec.ClusterName)
+			gwDev := ctx.GetDeviceFromResults(gw.Name, vxLocal)
+			gwIP, _, err := net.ParseCIDR(gwDev.Addr)
+			if err != nil {
+				klog.Warning("cannot parse gw dev addr, nodeName: %s, devName: %s", gw.Name, vxLocal)
+				continue
+			}
+
+			ctx.Results[n.Name].Routes = append(ctx.Results[n.Name].Routes, v1alpha1.Route{
+				CIDR: cidr,
+				Gw:   gwIP.String(),
+				Dev:  vxLocal,
+			})
+		}
+	}
 }
