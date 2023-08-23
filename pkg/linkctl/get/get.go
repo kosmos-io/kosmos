c37f63c239df000dfc9f10019ca172f6fd544ae6
ONE7live <wangqi_yewu@cmss.chinamobile.com>
2023-08-23 17:15:49 +0800

    feat: linkctl support using the get command
    Signed-off-by: ONE7live <wangqi_yewu@cmss.chinamobile.com>

---
+++ pkg/linkctl/get/get.go
@@ -0,0 +1,106 @@
+package get
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	ctlget "k8s.io/kubectl/pkg/cmd/get"
+	ctlutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/util/i18n"
+
+	"github.com/kosmos.io/clusterlink/pkg/generated/clientset/versioned"
+)
+
+const (
+	ClustersGroupResource     = "clusters.clusterlink.io"
+	ClusterNodesGroupResource = "clusternodes.clusterlink.io"
+)
+
+type CommandGetOptions struct {
+	Cluster     string
+	ClusterNode string
+
+	GetOptions *ctlget.GetOptions
+
+	clusterLinkClient *versioned.Clientset
+}
+
+// NewCmdGet Display resources from the Clusterlink control plane.
+func NewCmdGet(f ctlutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	o := NewCommandGetOptions(streams)
+
+	cmd := &cobra.Command{
+		Use:                   fmt.Sprintf("get [(-o|--output=)%s] (TYPE[.VERSION][.GROUP] [NAME | -l label] | TYPE[.VERSION][.GROUP]/NAME ...) [flags]", strings.Join(o.GetOptions.PrintFlags.AllowedFormats(), "|")),
+		Short:                 i18n.T("Display resources from the Clusterlink control plane"),
+		Long:                  "",
+		Example:               "",
+		SilenceUsage:          true,
+		DisableFlagsInUseLine: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctlutil.CheckErr(o.Complete(f, cmd, args))
+			ctlutil.CheckErr(o.Validate())
+			ctlutil.CheckErr(o.Run(f, cmd, args))
+			return nil
+		},
+	}
+
+	o.GetOptions.PrintFlags.AddFlags(cmd)
+
+	return cmd
+}
+
+// NewCommandGetOptions returns a CommandGetOptions.
+func NewCommandGetOptions(streams genericclioptions.IOStreams) *CommandGetOptions {
+	getOptions := ctlget.NewGetOptions("linkctl", streams)
+	return &CommandGetOptions{
+		GetOptions: getOptions,
+	}
+}
+
+func (o *CommandGetOptions) Complete(f ctlutil.Factory, cmd *cobra.Command, args []string) error {
+	config, err := f.ToRESTConfig()
+	if err != nil {
+		return fmt.Errorf("linkctl get complete error, generate rest config failed: %s", err)
+	}
+
+	o.clusterLinkClient, err = versioned.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("linkctl get complete error, create clusterlink client failed: %s", err)
+	}
+
+	err = o.GetOptions.Complete(f, cmd, args)
+	if err != nil {
+		return fmt.Errorf("linkctl get complete error, options failed: %s", err)
+	}
+
+	return nil
+}
+
+func (o *CommandGetOptions) Validate() error {
+	err := o.GetOptions.Validate()
+	if err != nil {
+		return fmt.Errorf("linkctl get validate error, options failed: %s", err)
+	}
+
+	return nil
+}
+
+func (o *CommandGetOptions) Run(f ctlutil.Factory, cmd *cobra.Command, args []string) error {
+	for i := range args {
+		switch args[i] {
+		case "cluster", "clusters":
+			args[i] = ClustersGroupResource
+		case "clusternode", "clusternodes":
+			args[i] = ClusterNodesGroupResource
+		}
+	}
+
+	err := o.GetOptions.Run(f, cmd, args)
+	if err != nil {
+		return fmt.Errorf("linkctl get run error, options failed: %s", err)
+	}
+
+	return nil
+}
---
+++ pkg/linkctl/linkctl.go
@@ -0,0 +1,79 @@
+package linkctl
+
+import (
+	"flag"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog/v2"
+	ctlutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/util/i18n"
+	"k8s.io/kubectl/pkg/util/templates"
+
+	"github.com/kosmos.io/clusterlink/pkg/linkctl/get"
+)
+
+// DefaultConfigFlags It composes the set of values necessary for obtaining a REST client config with default values set.
+var DefaultConfigFlags = genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag().WithDiscoveryBurst(300).WithDiscoveryQPS(50.0)
+
+// NewLinkCtlCommand creates the `linkctl` command with arguments.
+func NewLinkCtlCommand() *cobra.Command {
+	cmds := &cobra.Command{
+		Use:   "linkctl",
+		Short: i18n.T("linkctl controls a Kubernetes cluster network"),
+		Long:  templates.LongDesc(`linkctl controls a Kubernetes cluster network.`),
+		RunE:  runHelp,
+	}
+
+	klog.InitFlags(flag.CommandLine)
+
+	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	cmds.PersistentFlags().AddFlagSet(pflag.CommandLine)
+
+	if err := flag.CommandLine.Parse(nil); err != nil {
+		klog.Warning(err)
+	}
+
+	f := ctlutil.NewFactory(DefaultConfigFlags)
+	ioStreams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+
+	groups := templates.CommandGroups{
+		{
+			Message: "Basic Commands:",
+			Commands: []*cobra.Command{
+				get.NewCmdGet(f, ioStreams),
+			},
+		},
+		//{
+		//	Message: "Install Commands:",
+		//	Commands: []*cobra.Command{
+		//		install.NewCmdInstall(f, ioStreams),
+		//		uninstall.NewCmdUninstall(f, ioStreams),
+		//	},
+		//},
+		//{
+		//	Message: "Cluster Member Join/UnJoin Commands:",
+		//	Commands: []*cobra.Command{
+		//		join.NewCmdJoin(),
+		//		unjoin.NewCmdUnJoin(),
+		//	},
+		//},
+		//{
+		//	Message: "Cluster Doctor/Floater Commands:",
+		//	Commands: []*cobra.Command{
+		//		floater.NewCmdDoctor(),
+		//	},
+		//},
+	}
+	groups.Add(cmds)
+
+	return cmds
+}
+
+func runHelp(cmd *cobra.Command, args []string) error {
+	return cmd.Help()
+}
