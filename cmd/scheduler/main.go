3a991f311e54830bb3f6ff688ce0b3caa7dd3cca
duanmengkk <duanmeng_yewu@cmss.chinamobile.com>
2023-09-11 19:34:46 +0800

    add scheduler taint logic for knode
    Signed-off-by: duanmengkk <duanmeng_yewu@cmss.chinamobile.com>

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -5,13 +5,17 @@ import (
 
 	"k8s.io/component-base/cli"
 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+
+	"github.com/kosmos.io/kosmos/pkg/scheduler/lifted/plugins/knodetainttoleration"
 )
 
 func main() {
 	// Register custom plugins to the scheduler framework.
 	// Later they can consist of scheduler profile(s) and hence
 	// used by various kinds of workloads.
-	command := app.NewSchedulerCommand()
+	command := app.NewSchedulerCommand(
+		app.WithPlugin(knodetainttoleration.Name, knodetainttoleration.New),
+	)
 
 	code := cli.Run(command)
 	os.Exit(code)
