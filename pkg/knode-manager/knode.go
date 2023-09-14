a2a392c71265bb770cb6536277870f41803156f4
wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>
2023-09-14 16:49:58 +0800

    feat: added node-controller and node adapter implementations
    Signed-off-by: wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>

--- pkg/knode-manager/knode.go
+++ pkg/knode-manager/knode.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 
 	"github.com/pkg/errors"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 	klogv2 "k8s.io/klog/v2"
 
 	"github.com/kosmos.io/kosmos/cmd/knode-manager/app/config"
@@ -14,6 +13,7 @@ import (
 	"github.com/kosmos.io/kosmos/pkg/knode-manager/adapters"
 	k8sadapter "github.com/kosmos.io/kosmos/pkg/knode-manager/adapters/k8s"
 	"github.com/kosmos.io/kosmos/pkg/knode-manager/controllers"
+	"github.com/kosmos.io/kosmos/pkg/knode-manager/utils"
 )
 
 type Knode struct {
@@ -21,40 +21,51 @@ type Knode struct {
 	nodeController *controllers.NodeController
 }
 
-func NewKnode(_ context.Context, knode *kosmosv1alpha1.Knode, c *config.Opts) (*Knode, error) {
-	kubeconfig, err := clientcmd.BuildConfigFromFlags("", c.KubeConfigPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to build master kubeconfig: %v", err)
+func NewKnode(ctx context.Context, knode *kosmosv1alpha1.Knode, c *config.Opts) (*Knode, error) {
+	if len(knode.Spec.Kubeconfig) == 0 {
+		return nil, fmt.Errorf("kubeconfig of knode %s is empty", knode.Name)
 	}
-	kubeconfig.QPS, kubeconfig.Burst = c.KubeAPIQPS, c.KubeAPIBurst
-	mClient, err := kubernetes.NewForConfig(kubeconfig)
+
+	mClient, err := utils.NewClientFromConfigPath(c.KubeConfigPath, func(config *rest.Config) {
+		config.QPS = c.KubeAPIQPS
+		config.Burst = c.KubeAPIBurst
+	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to new master clientset: %v", err)
+		return nil, fmt.Errorf("could not build clientset for master cluster: %v", err)
+	}
+
+	wClient, err := utils.NewClientFromBytes(knode.Spec.Kubeconfig, func(config *rest.Config) {
+		config.QPS = knode.Spec.KubeAPIQPS
+		config.Burst = knode.Spec.KubeAPIBurst
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not build clientset for worker cluster %s: %v", knode.Name, err)
 	}
 
 	var podAdapter adapters.PodHandler
 	var nodeAdapter adapters.NodeHandler
 	if knode.Spec.Type == kosmosv1alpha1.K8sAdapter {
 		initConfig := k8sadapter.PodAdapterConfig{}
-
 		podAdapter, err = k8sadapter.NewPodAdapter(initConfig, "", &k8sadapter.ClientConfig{}, true)
 		if err != nil {
 			return nil, err
 		}
-		nodeAdapter, err = k8sadapter.NewNodeAdapter()
+
+		nodeAdapter, err = k8sadapter.NewNodeAdapter(ctx, knode, wClient, mClient, c)
 		if err != nil {
 			return nil, err
 		}
+	}
+
+	dummyNode := controllers.BuildDummyNode(ctx, knode, nodeAdapter)
+	nc, err := controllers.NewNodeController(nodeAdapter, mClient, dummyNode)
+	if err != nil {
+		return nil, err
 	}
 
 	pc, err := controllers.NewPodController(controllers.PodConfig{
 		PodHandler: podAdapter,
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	nc, err := controllers.NewNodeController(nodeAdapter, mClient)
 	if err != nil {
 		return nil, err
 	}
