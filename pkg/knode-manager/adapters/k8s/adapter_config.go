4e623ddc6bb37ba8a2c8461c85421153e11413fc
wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>
2023-09-24 15:19:32 +0800

    fix: pv/pvc debug
    Signed-off-by: wangyizhi1 <wangyizhi1_yewu@cmss.chinamobile.com>

--- pkg/knode-manager/adapters/k8s/adapter_config.go
+++ pkg/knode-manager/adapters/k8s/adapter_config.go
@@ -11,14 +11,12 @@ type AdapterConfig struct {
 	Client kubernetes.Interface
 	Master kubernetes.Interface
 
-	PodInformer                   v1.PodInformer
-	NamespaceInformer             v1.NamespaceInformer
-	NodeInformer                  v1.NodeInformer
-	ConfigmapInformer             v1.ConfigMapInformer
-	SecretInformer                v1.SecretInformer
-	ServiceInformer               v1.ServiceInformer
-	PersistentVolumeInformer      v1.PersistentVolumeInformer
-	PersistentVolumeClaimInformer v1.PersistentVolumeClaimInformer
+	PodInformer       v1.PodInformer
+	NamespaceInformer v1.NamespaceInformer
+	NodeInformer      v1.NodeInformer
+	ConfigmapInformer v1.ConfigMapInformer
+	SecretInformer    v1.SecretInformer
+	ServiceInformer   v1.ServiceInformer
 
 	ResourceManager *manager.ResourceManager
 }
--- pkg/knode-manager/utils/util.go
+++ pkg/knode-manager/utils/util.go
@@ -8,3 +8,24 @@ func SetObjectGlobal(obj *metav1.ObjectMeta) {
 	}
 	obj.Annotations[KosmosGlobalLabel] = "true"
 }
+
+func IsObjectGlobal(obj *metav1.ObjectMeta) bool {
+	if obj.Annotations == nil {
+		return false
+	}
+
+	if obj.Annotations[KosmosGlobalLabel] == "true" {
+		return true
+	}
+
+	return false
+}
+
+func CheckGlobalLabelEqual(obj, clone *metav1.ObjectMeta) bool {
+	oldGlobal := IsObjectGlobal(obj)
+	if !oldGlobal {
+		return false
+	}
+	newGlobal := IsObjectGlobal(clone)
+	return newGlobal
+}
