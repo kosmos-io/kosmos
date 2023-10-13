c9a062a175b1ef95e820c1ae3b5b2f93dd7fef26
baoyinghai <baoyinghai@cmss.chinamobile.com>
2023-10-13 11:16:05 +0800

    feat: enable loose mode for flannel interface

--- pkg/clusterlink/network/env.go
+++ pkg/clusterlink/network/env.go
@@ -2,10 +2,13 @@ package network
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"strings"
 
 	ipt "github.com/coreos/go-iptables/iptables"
 	"github.com/pkg/errors"
+	"github.com/vishvananda/netlink"
 
 	"github.com/kosmos.io/kosmos/pkg/clusterlink/network/iptables"
 )
@@ -65,7 +68,27 @@ func EnableLooseModeByIFaceNmae(ifaceName string) error {
 }
 
 func EnableDisableIPV6ByIFaceNmae(ifaceName string) error {
-	// Enable loose mode (rp_filter=2) reverse path filtering on the vxlan interface.
+	// Enable ipv6 (disable_ipv6=0)
 	err := setSysctl("/proc/sys/net/ipv6/conf/"+ifaceName+"/disable_ipv6", []byte("0"))
 	return errors.Wrapf(err, "unable to update disable_ipv6 proc entry for interface %q", ifaceName)
+}
+
+// set flannel interface (rp_filter=2)
+func EnableLooseModeForFlannel() error {
+	links, err := netlink.LinkList()
+	if err != nil {
+		return err
+	}
+
+	var errs error
+	for _, link := range links {
+		linkName := link.Attrs().Name
+		if strings.HasPrefix(linkName, FLANNEL_DEV_NAME_PREFIX) {
+			if err := setSysctl("/proc/sys/net/ipv4/conf/"+linkName+"/rp_filter", []byte("2")); err != nil {
+				errs = errors.Wrapf(errors.Wrapf(err, "unable to update flannel rp_filter proc entry for interface %q", linkName), fmt.Sprint(errs))
+			}
+		}
+	}
+
+	return errs
 }
