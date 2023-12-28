cf5d4b89674451eeb3a8e22c0e913de24e413926
duanmengkk <duanmeng_yewu@cmss.chinamobile.com>
2023-12-28 13:38:38 +0800

    add finalizer for mcs and fix some bug
    Signed-off-by: duanmengkk <duanmeng_yewu@cmss.chinamobile.com>

--- hack/rune2e.sh
+++ hack/rune2e.sh
@@ -7,7 +7,6 @@ set -o pipefail
 KUBECONFIG_PATH=${KUBECONFIG_PATH:-"${HOME}/.kube"}
 export KUBECONFIG=$KUBECONFIG_PATH/"config"
 
-
 E2E_NAMESPACE="kosmos-e2e"
 HOST_CLUSTER_NAME="cluster-host"
 MEMBER1_CLUSTER_NAME="cluster-member1"
@@ -33,7 +32,10 @@ util::wait_for_condition "mcs of member2 are ready" \
 nginx_service_ip=$(kubectl -n kosmos-e2e get svc nginx-service -o=jsonpath='{.spec.clusterIP}')
 
 # e2e test for access nginx service
-sleep 100 && docker exec -i ${HOST_CLUSTER_NAME}-control-plane sh -c "curl -sSf -m 5 ${nginx_service_ip}:80" && echo "success" || { echo "fail"; exit 1; }
+sleep 100 && docker exec -i ${HOST_CLUSTER_NAME}-control-plane sh -c "curl -sSf -m 5 ${nginx_service_ip}:80" && echo "success" || {
+  echo "fail"
+  exit 1
+}
 
 # e2e for mysql-operator
 kubectl --context="kind-cluster-host" apply -f "${ROOT}"/../test/e2e/deploy/mysql-operator
@@ -45,8 +47,10 @@ util::wait_for_condition "mysql operator are ready" \
 kubectl --context="kind-${HOST_CLUSTER_NAME}" apply -f "${ROOT}"/../test/e2e/deploy/cr
 
 sleep 240
-kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -n kosmos-e2e
-
+echo "主集群e2e pod"
+kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -n kosmos-e2e -o wide
+echo "主集群所有 pod"
+kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -A
 echo "集群2 servicimport"
 kubectl --context=kind-${MEMBER2_CLUSTER_NAME} -n kosmos-e2e get serviceimports
 echo "集群1 servicimport"
@@ -57,7 +61,10 @@ echo "集群1 esp"
 kubectl --context=kind-${MEMBER1_CLUSTER_NAME} -n kosmos-e2e get endpointslices
 echo "主集群 esp"
 kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get endpointslices
-
+echo "主集群 mysql init容器日志"
+kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e logs mysql-cluster-e2e-mysql-0 init
+echo "主集群 mysql mysql容器日志"
+kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e logs mysql-cluster-e2e-mysql-0 mysql
 #util::wait_for_condition "mysql cr are ready" \
 #  "[ \$(kubectl get pods -n kosmos-e2e --field-selector=status.phase=Running --no-headers | wc -l) -eq 2 ]" \
 #  1200
