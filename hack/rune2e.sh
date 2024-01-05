c7830965f0066f1eba2fff8cac28a9850ce635fc
duanmengkk <duanmeng_yewu@cmss.chinamobile.com>
2024-01-05 09:17:56 +0800

    fix
    Signed-off-by: duanmengkk <duanmeng_yewu@cmss.chinamobile.com>

--- hack/rune2e.sh
+++ hack/rune2e.sh
@@ -16,75 +16,39 @@ ROOT="$(dirname "${BASH_SOURCE[0]}")"
 source "${ROOT}/util.sh"
 
 # e2e for nginx and mcs
-#kubectl --context="kind-${HOST_CLUSTER_NAME}" apply -f "${ROOT}"/../test/e2e/deploy/nginx
-#util::wait_for_condition "nginx are ready" \
-#  "kubectl --context=kind-${HOST_CLUSTER_NAME} -n ${E2E_NAMESPACE} get pod -l app=nginx | awk 'NR>1 {if (\$3 == \"Running\") exit 0; else exit 1; }'" \
-#  120
+kubectl --context="kind-${HOST_CLUSTER_NAME}" apply -f "${ROOT}"/../test/e2e/deploy/nginx
+util::wait_for_condition "nginx are ready" \
+  "kubectl --context=kind-${HOST_CLUSTER_NAME} -n ${E2E_NAMESPACE} get pod -l app=nginx | awk 'NR>1 {if (\$3 == \"Running\") exit 0; else exit 1; }'" \
+  120
 
-#util::wait_for_condition "mcs of member1 are ready" \
-#  "[ \$(kubectl --context=kind-${MEMBER1_CLUSTER_NAME} -n ${E2E_NAMESPACE} get endpointslices.discovery.k8s.io --no-headers -l kubernetes.io\/service-name=nginx-service | wc -l) -eq 1 ] " \
-#  120
+util::wait_for_condition "mcs of member1 are ready" \
+  "[ \$(kubectl --context=kind-${MEMBER1_CLUSTER_NAME} -n ${E2E_NAMESPACE} get endpointslices.discovery.k8s.io --no-headers -l kubernetes.io\/service-name=nginx-service | wc -l) -eq 1 ] " \
+  120
 
-#util::wait_for_condition "mcs of member2 are ready" \
-#  "[ \$(kubectl --context=kind-${MEMBER2_CLUSTER_NAME} -n ${E2E_NAMESPACE} get endpointslices.discovery.k8s.io --no-headers -l kubernetes.io\/service-name=nginx-service | wc -l) -eq 1 ] " \
-#  120
+util::wait_for_condition "mcs of member2 are ready" \
+  "[ \$(kubectl --context=kind-${MEMBER2_CLUSTER_NAME} -n ${E2E_NAMESPACE} get endpointslices.discovery.k8s.io --no-headers -l kubernetes.io\/service-name=nginx-service | wc -l) -eq 1 ] " \
+  120
 
-#nginx_service_ip=$(kubectl -n kosmos-e2e get svc nginx-service -o=jsonpath='{.spec.clusterIP}')
+nginx_service_ip=$(kubectl -n kosmos-e2e get svc nginx-service -o=jsonpath='{.spec.clusterIP}')
 
 # e2e test for access nginx service
-#sleep 100 && docker exec -i ${HOST_CLUSTER_NAME}-control-plane sh -c "curl -sSf -m 5 ${nginx_service_ip}:80" && echo "success" || {
-#  echo "fail"
-#  exit 1
-#}
+sleep 100 && docker exec -i ${HOST_CLUSTER_NAME}-control-plane sh -c "curl -sSf -m 5 ${nginx_service_ip}:80" && echo "success" || {
+  echo "fail"
+  exit 1
+}
 
-# e2e for mysql-operator
-kubectl --context="kind-cluster-host" apply -f "${ROOT}"/../test/e2e/deploy/mysql-operator
-util::wait_for_condition "mysql operator are ready" \
-  "kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -n mysql-operator mysql-operator-0 | awk 'NR>1 {if (\$3 == \"Running\") exit 0; else exit 1; }'" \
-  300
-kubectl --context=kind-${HOST_CLUSTER_NAME} create ns kosmos-e2e
-#kubectl --context="kind-cluster-host" exec -it /bin/sh -c
-kubectl --context="kind-${HOST_CLUSTER_NAME}" apply -f "${ROOT}"/../test/e2e/deploy/cr
-
-sleep 240
-echo "主集群e2e pod"
-kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -n kosmos-e2e -o wide
-echo "主集群所有 pod"
-kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -A
-echo "集群2 servicimport"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get serviceimports
-echo "集群1 servicimport"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get serviceimports
-echo "集群2 esp"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get endpointslices
-echo "集群1 esp"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get endpointslices
-echo "主集群 esp"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get endpointslices
-
-echo "集群1 mysql init容器日志"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e logs mysql-cluster-e2e-mysql-0 init
-echo "集群1 mysql mysql容器日志"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e logs mysql-cluster-e2e-mysql-0 mysql
-
-echo "集群1 mysql pvc"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get pvc
-echo "集群1 mysql pv"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get pv
-
-echo "集群1 所有 pod"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e get pvc
-
-echo "集群1 mysql init容器旧日志"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e logs -p mysql-cluster-e2e-mysql-0 init
-echo "集群1 mysql mysql容器旧日志"
-kubectl --context=kind-${HOST_CLUSTER_NAME} -n kosmos-e2e logs -p mysql-cluster-e2e-mysql-0 mysql
+## e2e for mysql-operator
+#kubectl --context="kind-cluster-host" apply -f "${ROOT}"/../test/e2e/deploy/mysql-operator
+#util::wait_for_condition "mysql operator are ready" \
+#  "kubectl --context=kind-${HOST_CLUSTER_NAME} get pods -n mysql-operator mysql-operator-0 | awk 'NR>1 {if (\$3 == \"Running\") exit 0; else exit 1; }'" \
+#  300
+#kubectl --context="kind-${HOST_CLUSTER_NAME}" apply -f "${ROOT}"/../test/e2e/deploy/cr
 
 #util::wait_for_condition "mysql cr are ready" \
 #  "[ \$(kubectl get pods -n kosmos-e2e --field-selector=status.phase=Running --no-headers | wc -l) -eq 2 ]" \
 #  1200
 
-echo "E2e test of mysql-operator success"
+#echo "E2e test of mysql-operator success"
 
 # Install ginkgo
 GO111MODULE=on go install github.com/onsi/ginkgo/v2/ginkgo
