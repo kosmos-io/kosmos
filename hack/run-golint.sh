398624a68e7da96efef807a757f27c2cbd4eecf4
zhaozhengwei <35306739+zhaozhengwei@users.noreply.github.com>
2024-01-25 17:30:16 +0800

    Update run-golint.sh
    test

--- hack/run-golint.sh
+++ hack/run-golint.sh
@@ -7,8 +7,8 @@ set -o pipefail
 # Function to install go-lint if it is not already installed
 echo "go lint is not installed. Installing..."
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/
 #GOLINT_PATH=$(go env GOPATH)/bin
-bash "${SCRIPT_ROOT}/hack/install-golint.sh -b /home/runner/go/bin v1.54.2"
+bash "${SCRIPT_ROOT}/install-golint.sh -b /home/runner/go/bin v1.54.2"
 
 echo "go lint is installed and the version is correct."
