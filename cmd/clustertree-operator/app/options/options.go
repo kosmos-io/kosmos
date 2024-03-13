46a5a67d42f4142bb6e7931bc3f74c1df2bfc93c
GreatLazyMan <dongweiguo_yewu@cmss.chinamobile.com>
2024-03-13 17:43:22 +0800

    title: virtualcluster join controller could delete useless node when update virtualcluster
    Signed-off-by: dongweiguo <dongweiguo_yewu@cmss.chinamobile.com>

--- cmd/clustertree-operator/app/options/options.go
+++ cmd/clustertree-operator/app/options/options.go
@@ -9,8 +9,9 @@ import (
 )
 
 type Options struct {
-	LeaderElection    componentbaseconfig.LeaderElectionConfiguration
-	KubernetesOptions KubernetesOptions
+	LeaderElection             componentbaseconfig.LeaderElectionConfiguration
+	KubernetesOptions          KubernetesOptions
+	AllowNodeOwnbyMulticluster bool
 }
 
 type KubernetesOptions struct {
@@ -37,10 +38,11 @@ func (o *Options) AddFlags(flags *pflag.FlagSet) {
 	}
 
 	flags.BoolVar(&o.LeaderElection.LeaderElect, "leader-elect", true, "Start a leader election client and gain leadership before executing the main loop. Enable this when running replicated components for high availability.")
-	flags.StringVar(&o.LeaderElection.ResourceName, "leader-elect-resource-name", "network-manager", "The name of resource object that is used for locking during leader election.")
+	flags.StringVar(&o.LeaderElection.ResourceName, "leader-elect-resource-name", "clustertree-operator", "The name of resource object that is used for locking during leader election.")
 	flags.StringVar(&o.LeaderElection.ResourceNamespace, "leader-elect-resource-namespace", utils.DefaultNamespace, "The namespace of resource object that is used for locking during leader election.")
 	flags.Float32Var(&o.KubernetesOptions.QPS, "kube-qps", 40.0, "QPS to use while talking with kube-apiserver.")
 	flags.IntVar(&o.KubernetesOptions.Burst, "kube-burst", 60, "Burst to use while talking with kube-apiserver.")
 	flags.StringVar(&o.KubernetesOptions.KubeConfig, "kubeconfig", "", "Path for kubernetes kubeconfig file, if left blank, will use in cluster way.")
 	flags.StringVar(&o.KubernetesOptions.Master, "master", "", "Used to generate kubeconfig for downloading, if not specified, will use host in kubeconfig.")
+	flags.BoolVar(&o.AllowNodeOwnbyMulticluster, "multiowner", false, "Allow node own by multicluster or not.")
 }
