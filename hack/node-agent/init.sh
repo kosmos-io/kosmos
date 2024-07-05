77af6a50e0fe10bec37a7967e7de7efe8d86a0e8
wangdepeng <wangdepeng_yewu@cmss.chinamobile.com>
2024-07-05 21:11:56 +0800

    feat: use cobra and viper to refactor the node agent
    Signed-off-by: wangdepeng <wangdepeng_yewu@cmss.chinamobile.com>
    (cherry picked from commit 2b3c1378699f6c3bcd1668aa6fb314a4031b620e)

--- hack/node-agent/init.sh
+++ hack/node-agent/init.sh
@@ -2,7 +2,7 @@
 
 WEB_USER="$WEB_USER" sed -i 's/^WEB_USER=.*/WEB_USER='"$WEB_USER"'/' /app/agent.env
 WEB_PASS="$WEB_PASS" sed -i 's/^WEB_PASS=.*/WEB_PASS='"$WEB_PASS"'/' /app/agent.env
-sha256sum /app/node-agent > node-agent.sum
-sha256sum /host-path/node-agent >> node-agent.sum
+sha256sum /app/node-agent > /app/node-agent.sum
+sha256sum /host-path/node-agent >> /app/node-agent.sum
 rsync -avz /app/ /host-path/
 cp /app/node-agent.service /host-systemd/node-agent.service
