fa4f60aad90412c196b8dd0f977016b58c9010fa
gaoyuan <gaoyuan@cmss.chinamobile.com>
2024-11-27 15:16:36 +0800

    update node-agent Heartbeat
    Signed-off-by: gaoyuan <gaoyuan@cmss.chinamobile.com>

--- hack/node-agent/init.sh
+++ hack/node-agent/init.sh
@@ -3,7 +3,9 @@
 WEB_USER="$WEB_USER" sed -i 's/^WEB_USER=.*/WEB_USER="'"$WEB_USER"'"/' /app/agent.env
 WEB_PASS="$WEB_PASS" sed -i 's/^WEB_PASS=.*/WEB_PASS="'"$WEB_PASS"'"/' /app/agent.env
 WEB_PORT="$WEB_PORT" sed -i 's/^WEB_PORT=.*/WEB_PORT="'"$WEB_PORT"'"/' /app/agent.env
+NODE_NAME="$NODE_NAME" sed -i 's/^NODE_NAME=.*/NODE_NAME="'"$NODE_NAME"'"/' /app/agent.env
+
 sha256sum /app/node-agent > /app/node-agent.sum
 sha256sum /host-path/node-agent >> /app/node-agent.sum
 rsync -avz /app/ /host-path/
-cp /app/node-agent.service /host-systemd/node-agent.service
+cp /app/node-agent.service /host-systemd/node-agent.service 
