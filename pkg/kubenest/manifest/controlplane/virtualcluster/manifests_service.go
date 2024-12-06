062f9068c9a843e8def47cd2a2e8b828309bc75d
qiuwei <qiuwei_yewu@cmss.chinamobile.com>
2024-12-06 09:31:38 +0800

    feat: external eps support dualstack and add unittests
    Signed-off-by: qiuwei <qiuwei_yewu@cmss.chinamobile.com>

--- pkg/kubenest/manifest/controlplane/virtualcluster/manifests_service.go
+++ pkg/kubenest/manifest/controlplane/virtualcluster/manifests_service.go
@@ -6,13 +6,23 @@ apiVersion: v1
 kind: Service
 metadata:
   name: api-server-external-service
-  namespace: default
+  namespace: kosmos-system
 spec:
+  ipFamilies:
+    {{- if .IPv4 }} 
+    - IPv4
+    {{- end }}
+    {{- if .IPv6 }} 
+    - IPv6
+    {{- end }}
+  ipFamilyPolicy: PreferDualStack
   type: NodePort
   ports:
-    - protocol: TCP
+    - name: https
+      protocol: TCP
       port: {{ .ServicePort }}
       targetPort: {{ .ServicePort }}
       nodePort: 30443
+  sessionAffinity: None
 `
 )
